b79624397b5f88b5720b3127649678cb19ccaca8
prince <princekushwaha2833@gmail.com>
2020-07-26 00:58:36 +0530

    Updates

---
+++ precache-manifest.ed194ebdaac63b56d9921300b2070cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a54bbaa5ca15a902b5468742ce916e",
+    "url": "/twitto/index.html"
+  },
+  {
+    "revision": "1933769fbc02b4f11128",
+    "url": "/twitto/static/css/main.06ff72b0.chunk.css"
+  },
+  {
+    "revision": "647e6b9bee314f767bf5",
+    "url": "/twitto/static/js/2.b9c8a235.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/twitto/static/js/2.b9c8a235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1933769fbc02b4f11128",
+    "url": "/twitto/static/js/main.2d8ed8d5.chunk.js"
+  },
+  {
+    "revision": "fe492a02d979b14eefe9",
+    "url": "/twitto/static/js/runtime-main.52c53d72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitto/static/media/logo.5d5d9eef.svg"
+  }
+]);
